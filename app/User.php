ed739a5ac4266b7b0668ef0c0b85fde262186028
Axel <axosar@gmail.com>
2020-07-15 13:55:38 -0500

    Post create - 1/2

--- app/User.php
+++ app/User.php
@@ -39,7 +39,7 @@ class User extends Authenticatable
 
     public function address(){
         return $this->hasOne('App\Address');
-    };
+    }
 
     public function posts(){
         return $this.hasMany('App\Post');
